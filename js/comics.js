db56132138d78f5f9bff0b40635113ff5ff854e6
Eduardo Irías <eduardo22i@me.com>
2014-06-08 22:14:14 -0600

    Sounds for frames
    - Sounds attached to frames
    - JS Comics (Simulated)

---
+++ js/comics.js
@@ -0,0 +1,34 @@
+var actComic = 1;
+
+var pages = 0;
+
+function getPages () {
+	
+	switch (actComic) {
+		case 1:
+			pages = 3;
+			return 	3;
+	}
+		
+}
+
+function getPagesMaps (page, actComic) {
+	
+	switch (actComic) {
+		case 1:
+			switch (page) {
+			case 1:
+				return 	'<area shape="rect" coords="57,67,934,974" href="javascript:playSound(1)"> <area shape="rect" coords="58,995,409,1244" href="javascript:playSound(2)"> <area shape="rect" coords="440,995,929,1247" href="javascript:playSound(3)">';
+				break;
+			case 2:
+			return '<area shape="rect" coords="65,47,422,420" href="javascript:playSound(4)"> <area shape="rect" coords="445,46,959,420" href="javascript:playSound(5)"><area shape="rect" coords="65,444,296,829" href="javascript:playSound(6)">    <area shape="rect" coords="315,444,603,829" href="javascript:playSound(7)">    <area shape="rect" coords="620,443,958,832" href="javascript:playSound(8)">    <area shape="rect" coords="65,849,452,1197" href="javascript:playSound(9)">    <area shape="rect" coords="498,849,958,1198" href="javascript:playSound(10)">';
+			break;
+			case 3:
+			return '<area shape="rect" coords="52,35,628,435" href="javascript:playSound(11)">    <area shape="rect" coords="643,31,965,438" href="javascript:playSound(12)">    <area shape="rect" coords="44,456,496,766" href="javascript:playSound(13)">    <area shape="rect" coords="514,458,963,764" href="javascript:playSound(14)">    <area shape="rect" coords="46,795,385,1242" href="javascript:playSound(15)">    <area shape="rect" coords="415,795,945,952" href="javascript:playSound(16)">    <area shape="rect" coords="415,977,946,1241" href="javascript:playSound(17)">';
+			break;
+		break;
+	}
+	}
+	
+		
+}
--- js/page.js
+++ js/page.js
@@ -1,8 +1,30 @@
-var actPg = 1;
+var actPg = 0;
+
+function init(x) {
+	
+	actComic = x;
+	getPages();
+	getPagesMaps(actComic);
+	
+}
 
 function cambiar() {
 
-	actPg++;
+	
 	var pag = document.getElementById("pag");
-	pag.src = "images/pag "+ (1+ (actPg%3))  +".jpg";
+	pag.src = "images/pag "+ (1+ (actPg%pages))  +".jpg";
+	
+	var pagArea = document.getElementById("pagMap");
+	pagArea.innerHTML = getPagesMaps ( (1+ (actPg%pages)),  actComic );
+	
+	actPg++;
+}
+
+function playSound(x) {
+	var pagaudioPlayer= document.getElementById("audioPlayer");
+	
+	audioPlayer.src = "sounds/cuadro " + x + ".mp3";
+	
+	audioPlayer.play();
+	
 }
